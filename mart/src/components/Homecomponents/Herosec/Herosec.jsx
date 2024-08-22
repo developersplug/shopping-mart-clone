afdb0b3c44dd259f5d7778f23e5b2aae0f1903cf
developersplug <bssegcuf472@gmail.com>
2024-08-22 11:06:30 +0500

    font

--- mart/src/components/Homecomponents/Herosec/Herosec.jsx
+++ mart/src/components/Homecomponents/Herosec/Herosec.jsx
@@ -5,8 +5,8 @@ const Herosec = () => {
     return (
         <div className=' hero flex items-center justify-center flex-col' >
             {/* <img src={herosection} alt="herosection image" className='w-full h-full object-cover' /> */}
-            <h2 className='text-[30px] text-white font-medium'>Purchase the Products You want</h2>
-            <p className='text-[22px] text-white font-medium'>It will Make us Happy and Proud to Serve you </p>
+            <h2 className='md:text-[30px] text-white mx-auto text-center text-[24px] font-medium'>Purchase the Products You want</h2>
+            <p className='mdt;ext-[22px] text-[17px] mx-auto text-center text-white font-medium'>It will Make us Happy and Proud to Serve you </p>
         </div>
     )
 }
